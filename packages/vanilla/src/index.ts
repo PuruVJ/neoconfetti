bc109e60641fb766be629993cee86dc926eb401e
Puru <awesomepuruvj@gmail.com>
2023-12-19 01:09:43 +0530

    Simplify!

--- packages/vanilla/src/index.ts
+++ packages/vanilla/src/index.ts
@@ -3,74 +3,52 @@ import { confetti, type ConfettiOptions, type ConfettiParticleShape } from '@neo
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export class Confetti {
-	/** Node */
-	private _n: HTMLElement;
+	#node: HTMLElement;
+	#options: ConfettiOptions = {};
 
-	/** Options */
-	private _o: ConfettiOptions = {};
-
-	/** Instance */
-	private _i?: ReturnType<typeof confetti>;
+	#instance?: ReturnType<typeof confetti>;
 
 	constructor(node: HTMLElement, options: ConfettiOptions = {}) {
-		this._n = node;
-		this._o = this._cp(options);
-
-		return new Proxy(this, {
-			set: (target, property: 'options', value: ConfettiOptions, receiver) => {
-				if (property === 'options') {
-					// Update the options and trigger confetti update
-					Reflect.set(target, '_options', this._cp(value), receiver);
-					target._uc();
-					return true;
-				}
-				return Reflect.set(target, property, value, receiver);
-			},
-			get: (target, property, receiver) => {
-				return Reflect.get(target, property, receiver);
-			},
-		});
+		this.#node = node;
+		this.options = options; // Use the setter to initialize options
 	}
 
 	/**
-	 * Create proxy
+	 * Options Proxy Creator
 	 */
-	private _cp = (options: ConfettiOptions) => {
+	#create_proxy = (options: ConfettiOptions) => {
 		return new Proxy(options, {
 			set: (target, property, value) => {
 				Reflect.set(target, property, value);
-				this._uc();
+				this.#update_confetti(); // Update confetti instance when options change
 				return true;
-			},
-			get: (target, property) => {
-				return Reflect.get(target, property);
 			},
 		});
 	};
 
-	/** Update confetti */
-	private _uc = () => {
-		if (this._i) {
-			this._i.update(this._o);
+	/** Update Confetti */
+	#update_confetti = () => {
+		if (this.#instance) {
+			this.#instance.update(this.#options);
 		}
 	};
 
 	get options() {
-		return this._o;
+		return this.#options;
 	}
 
 	set options(value: ConfettiOptions) {
-		this._o = this._cp(value);
-		this._uc();
+		this.#options = this.#create_proxy(value); // Initialize options with a proxy
+		this.#update_confetti(); // Update confetti instance on setting new options
 	}
 
 	async explode() {
-		this._i = confetti(this._n, this._o);
-		return sleep(this._o.duration ?? 3500);
+		this.#instance = confetti(this.#node, this.#options);
+		return sleep(this.#options.duration ?? 3500);
 	}
 
 	destroy() {
-		this._i?.destroy();
+		this.#instance?.destroy();
 	}
 }
 
