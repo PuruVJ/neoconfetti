c3e27468ca6375fe5dd216561f73da879a8b9acf
Puru <awesomepuruvj@gmail.com>
2023-12-20 00:10:47 +0530

    Update docs

--- scripts/sizes.ts
+++ scripts/sizes.ts
@@ -1,49 +1,53 @@
-// DOESN"T WORK YET
-/// <reference types="bun-types" />
 import { file as brotliSize } from 'brotli-size';
+import fg from 'fast-glob';
+import { readFileSync } from 'node:fs';
+import { mkdir, writeFile } from 'node:fs/promises';
 
-const packages = ['svelte'];
+async function main() {
+	const files = (
+		await fg(new URL('../packages/*/dist/min/index.js', import.meta.url).pathname)
+	).filter((path) => !path.includes('core'));
 
-const files = packages.map(
-	(p) => new URL(`../packages/${p}/dist/min/index.js`, import.meta.url).pathname
-);
+	const versions = (await fg(new URL('../packages/*/package.json', import.meta.url).pathname))
+		.filter((path) => !path.includes('core'))
+		.map((path) => {
+			const framework = /packages\/(?<framework>[^ $]*)\/package\.json/.exec(path)?.groups
+				?.framework!;
 
-const versions = await Promise.all(
-	packages.map(async (pkg) => {
-		const path = files[packages.indexOf(pkg)].replace('/dist/min/index.js', '/package.json');
+			return { framework, version: JSON.parse(readFileSync(path, 'utf-8')).version };
+		});
 
-		return { framework: pkg, version: JSON.parse(await Bun.file(path).text()).version };
-	})
-);
+	const contents = (
+		await Promise.all(
+			files.map(async (file) => {
+				const framework = /packages\/(?<framework>[^ $]*)\/dist/.exec(file)?.groups?.framework!;
+				const size = ((await brotliSize(file)) / 1024).toFixed(2);
 
-const contents = (
-	await Promise.all(
-		files.map(async (file) => {
-			const framework = /packages\/(?<framework>[^ $]*)\/dist/.exec(file)?.groups?.framework!;
-			const size = ((await brotliSize(file)) / 1024).toFixed(2);
+				return { framework, size };
+			})
+		)
+	).reduce(
+		(acc, { framework, size }) => ({
+			...acc,
+			[framework]: {
+				size,
+				version: versions.find(({ framework: vFw }) => vFw === framework)?.version,
+			},
+		}),
+		{}
+	);
+	console.log(contents);
+	console.table(Object.entries(contents).sort((a, b) => (a[0] > b[0] ? 1 : -1)));
 
-			return { framework, size };
-		})
-	)
-).reduce(
-	(acc, { framework, size }) => ({
-		...acc,
-		[framework]: {
-			size,
-			version: versions.find(({ framework: vFw }) => vFw === framework)?.version,
-		},
-	}),
-	{}
-);
+	// // Ensure folder if not exists
+	// try {
+	// 	await mkdir(new URL('../docs/src/data', import.meta.url).pathname);
+	// } catch (error) {}
 
-console.table(Object.entries(contents).sort((a, b) => (a[0] > b[0] ? 1 : -1)));
+	// writeFile(
+	// 	new URL('../docs/src/data/sizes.json', import.meta.url),
+	// 	JSON.stringify(contents, null, 2)
+	// );
+}
 
-// // Ensure folder if not exists
-// try {
-// 	await mkdir(new URL('../docs/src/data', import.meta.url).pathname);
-// } catch (error) {}
-
-// Bun.write(
-// 	new URL('../docs/src/data/sizes.json', import.meta.url),
-// 	JSON.stringify(contents, null, 2)
-// );
+main();
