41bc026440b593ae4d4bd2139117e7a08cb08557
Puru <awesomepuruvj@gmail.com>
2023-12-24 11:20:43 +0530

    Bump

--- packages/solid/demo/src/App.tsx
+++ packages/solid/demo/src/App.tsx
@@ -1,12 +1,45 @@
-import { createConfetti } from '@neoconfetti/solid';
-import { Component } from 'solid-js';
+import { ConfettiOptions, createConfetti } from '@neoconfetti/solid';
+import { Component, createEffect, createSignal } from 'solid-js';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const App: Component = () => {
 	const { confetti } = createConfetti();
+	const [options, setOptions] = createSignal<ConfettiOptions>({});
+	const [render, setRender] = createSignal(false);
+
+	createEffect(() => {
+		async function run() {
+			if (!render) return;
+
+			await sleep(2000);
+
+			setOptions((o) => ({
+				...o,
+				colors: ['red', 'blue'],
+				particleShape: 'circles',
+			}));
+
+			await sleep(600);
+
+			// Override entire options, re-triggers the confetti
+			setOptions({
+				particleCount: 200,
+				duration: 3500,
+				colors: ['#eee', 'black', 'hotpink' /* Sorry 😅 */],
+			});
+		}
+
+		run();
+	});
 
 	return (
 		<>
-			<div use:confetti style={{ translate: '10vh 10vh' }}></div>
+			<>
+				<button onClick={() => setRender(true)}>🎉</button>
+
+				{render() && <div use:confetti={options()} />}
+			</>
 		</>
 	);
 };
